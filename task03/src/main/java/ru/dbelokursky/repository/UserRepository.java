f2bd3b2505ac748bb248b8fe9ef5635e30003f3e
Dmitry Belokursky <dbelokursky@gmail.com>
2022-11-28 22:36:46 +0200

    Сделать простейший RESTful CRUD по созданию, удалению, просмотру и обновлению пользователей.

--- task03/src/main/java/ru/dbelokursky/repository/UserRepository.java
+++ task03/src/main/java/ru/dbelokursky/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package ru.dbelokursky.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.dbelokursky.rest.model.User;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends CrudRepository<User, String> {
 
-//  public User
 }
