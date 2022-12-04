9548358c11159ea80bb81c1bbf4c0a30e2dd8370
Dmitry Belokursky <dbelokursky@gmail.com>
2022-12-04 18:42:28 +0200

    Сделать простейший RESTful CRUD по созданию, удалению, просмотру и обновлению пользователей.

--- task02/src/main/java/ru/dbelokursky/microservice/Task02.java
+++ task02/src/main/java/ru/dbelokursky/microservice/Task02.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceApplication {
+public class Task02 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
+		SpringApplication.run(Task02.class, args);
 	}
 
 }
