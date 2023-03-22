1e9033766cffbeacd187df08f9d58757e1a10b77
Dmitry Belokursky <dbelokursky@gmail.com>
2023-03-22 22:51:34 +0200

    task06 auth service

--- task06_2/src/main/resources/schema.sql
+++ task06_2/src/main/resources/schema.sql
@@ -26,22 +26,51 @@ create table if not exists auth_user_auth_role
 );
 
 insert into auth_role (id, name, created, updated)
-values (1, 'ROLE_USER', '2023-02-26', '2023-02-26');
+SELECT 1, 'ROLE_USER', '2023-02-26', '2023-02-26'
+WHERE NOT EXISTS (SELECT 1 FROM auth_role WHERE id = 1);
 
 insert into auth_role (id, name, created, updated)
-values (2, 'ROLE_ADMIN', '2023-02-26', '2023-02-26');
+SELECT 2, 'ROLE_ADMIN', '2023-02-26', '2023-02-26'
+WHERE NOT EXISTS (SELECT 1 FROM auth_role WHERE id = 2);
 
 insert into auth_user(id, login, password)
-values (1, 'user', '$2a$12$TOPvPWHUeIJzoyFy.j8f8eVyyKKCjOV/8PJKTGQNcTFXmb3beiTxS');
+SELECT 1, 'user', '$2a$12$TOPvPWHUeIJzoyFy.j8f8eVyyKKCjOV/8PJKTGQNcTFXmb3beiTxS'
+WHERE NOT EXISTS (SELECT 1 FROM auth_user WHERE id = 1);
 
 insert into auth_user(id, login, password)
-values (2, 'admin', '$2a$12$2n0m/62GIUWyH4fWIKTADuFERPC2JkAETTsuU2IJorughaXF58Hsu');
+SELECT 2, 'admin', '$2a$12$2n0m/62GIUWyH4fWIKTADuFERPC2JkAETTsuU2IJorughaXF58Hsu'
+WHERE NOT EXISTS (SELECT 1 FROM auth_user WHERE id = 2);
+
 
 insert into auth_user_auth_role(auth_user, auth_role)
-values (1, 1);
+SELECT 1, 1
+WHERE NOT EXISTS (SELECT 1 FROM auth_user_auth_role WHERE auth_user = 1 and auth_role = 1);
 
 insert into auth_user_auth_role(auth_user, auth_role)
-values (2, 1);
+SELECT 2, 1
+WHERE NOT EXISTS (SELECT 1 FROM auth_user_auth_role WHERE auth_user = 2 and auth_role = 1);
 
 insert into auth_user_auth_role(auth_user, auth_role)
-values (2, 2);
+SELECT 2, 2
+WHERE NOT EXISTS (SELECT 1 FROM auth_user_auth_role WHERE auth_user = 2 and auth_role = 2);
+
+-- insert into auth_role (id, name, created, updated)
+-- values (1, 'ROLE_USER', '2023-02-26', '2023-02-26');
+
+-- insert into auth_role (id, name, created, updated)
+-- values (2, 'ROLE_ADMIN', '2023-02-26', '2023-02-26');
+
+-- insert into auth_user(id, login, password)
+-- values (1, 'user', '$2a$12$TOPvPWHUeIJzoyFy.j8f8eVyyKKCjOV/8PJKTGQNcTFXmb3beiTxS');
+
+-- insert into auth_user(id, login, password)
+-- values (2, 'admin', '$2a$12$2n0m/62GIUWyH4fWIKTADuFERPC2JkAETTsuU2IJorughaXF58Hsu');
+
+-- insert into auth_user_auth_role(auth_user, auth_role)
+-- values (1, 1);
+
+-- insert into auth_user_auth_role(auth_user, auth_role)
+-- values (2, 1);
+
+-- insert into auth_user_auth_role(auth_user, auth_role)
+-- values (2, 2);
